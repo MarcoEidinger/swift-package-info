b10266a66e6da022f1cad2744bab74426a951f7c
Marco Eidinger <eidingermarco@gmail.com>
2022-11-08 13:04:34 -0800

    uptake Swift 5.7

--- Package.swift
+++ Package.swift
@@ -1,22 +1,25 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "swift-package-info",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13)
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "SwiftPM", url: "https://github.com/SDGGiesbrecht/swift-package-manager.git", .exact("0.50302.0")),
+        .package(url: "https://github.com/SDGGiesbrecht/swift-package-manager.git", exact: "0.50700.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "swift-package-info",
-            dependencies: ["SwiftPM"]),
-        .testTarget(
-            name: "swift-package-infoTests",
-            dependencies: ["swift-package-info"]),
+            dependencies: [
+                .product(name: "SwiftPM", package: "swift-package-manager"),
+            ])
     ]
 )
--- Sources/swift-package-info/main.swift
+++ Sources/swift-package-info/main.swift
@@ -1,35 +1,26 @@
-import PackageModel
-import PackageLoading
-import PackageGraph
+import Basics
+import TSCBasic
 import Workspace
 
 // PREREQUISITES
 // ============
-// We will need to know where the Swift compiler is.
-let swiftCompiler: AbsolutePath = {
-    let string: String
-    #if os(macOS)
-    string = try! Process.checkNonZeroExit(args: "xcrun", "--sdk", "macosx", "-f", "swiftc").spm_chomp()
-    #else
-    string = try! Process.checkNonZeroExit(args: "which", "swiftc").spm_chomp()
-    #endif
-    return AbsolutePath(string)
-}()
-
 // We need a package to work with.
-// This assumes there is one in the current working directory:
-let packagePath = localFileSystem.currentWorkingDirectory!
+// This computes the path of this package root based on the file location
+let packagePath = AbsolutePath(#file).parentDirectory.parentDirectory.parentDirectory
 
 // LOADING
 // =======
-// Note:
 // There are several levels of information available.
 // Each takes longer to load than the level above it, but provides more detail.
-let diagnostics = DiagnosticsEngine()
-let manifest = try ManifestLoader.loadManifest(packagePath: packagePath, swiftCompiler: swiftCompiler, packageKind: .remote)
-let loadedPackage = try PackageBuilder.loadPackage(packagePath: packagePath, swiftCompiler: swiftCompiler, xcTestMinimumDeploymentTargets: [:], diagnostics: diagnostics)
-let graph = try Workspace.loadGraph(packagePath: packagePath, swiftCompiler: swiftCompiler, diagnostics: diagnostics)
+let observability = ObservabilitySystem({ print("\($0): \($1)") })
 
+let workspace = try Workspace(forRootPackage: packagePath)
+
+let manifest = try tsc_await { workspace.loadRootManifest(at: packagePath, observabilityScope: observability.topScope, completion: $0) }
+
+let package = try tsc_await { workspace.loadRootPackage(at: packagePath, observabilityScope: observability.topScope, completion: $0) }
+
+let graph = try workspace.loadPackageGraph(rootPath: packagePath, observabilityScope: observability.topScope)
 
 // EXAMPLES
 // ========
@@ -40,22 +31,9 @@ let targets = manifest.targets.map({ $0.name }).joined(separator: ", ")
 print("Targets:", targets)
 
 // Package
-let executables = loadedPackage.targets.filter({ $0.type == .executable }).map({ $0.name })
+let executables = package.targets.filter({ $0.type == .executable }).map({ $0.name })
 print("Executable targets:", executables)
 
 // PackageGraph
 let numberOfFiles = graph.reachableTargets.reduce(0, { $0 + $1.sources.paths.count })
 print("Total number of source files (including dependencies):", numberOfFiles)
-
-let numberOfOwnFiles = graph.reachableTargets.filter({ executables.contains($0.name) }).reduce(0, { $0 + $1.sources.paths.count })
-print("Total number of source files (no dependencies):", numberOfOwnFiles)
-
-// ========
-print("Reading Package.swift from \(packagePath.asURL.path):")
-do {
-    let manifestAsString = try String(contentsOfFile: packagePath.asURL.path.appending("/Package.swift"))
-    print(manifestAsString)
-    print("========")
-} catch {
-    print("Error")
-}
